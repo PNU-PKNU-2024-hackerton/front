ecd112740920a53f696b8b94cb0d0de60c412702
Gyehyun Park <gyehyunbak@gmail.com>
2024-08-17 15:15:56 +0900

    feature: 사이드바 반응형으로 구현

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Button";
-import { PlusCircle } from "lucide-react";
+import { Menu, PlusCircle, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import ExhibitionList, {
   ExhibitionListProps,
@@ -41,6 +41,7 @@ export default function Sidebar() {
   const navigate = useNavigate();
   const [exhibitionList, setExhibitionList] = useState<ExhibitionType[]>();
   const { isAuthenticated, username, setIsAuthenticated } = useUserStore();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     if (!isAuthenticated) return;
@@ -74,54 +75,77 @@ export default function Sidebar() {
   };
 
   return (
-    <div className="flex h-full w-full max-w-[300px] flex-col items-start justify-start gap-y-10 border-r border-gray-900 px-5 pt-8">
-      <a
-        className="text-2xl hover:cursor-pointer"
-        onClick={() => navigate("/")}
+    <div className="flex h-full">
+      <div className="absolute p-2 lg:hidden">
+        <Button
+          variant={"ghost"}
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
+          <Menu />
+        </Button>
+      </div>
+      <aside
+        className={`${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } fixed left-0 top-0 z-50 flex h-full w-full max-w-[300px] flex-col items-start justify-start gap-y-10 border-r border-gray-900 bg-white px-5 pt-8 transition-transform duration-300 ease-in-out lg:static lg:translate-x-0`}
       >
-        MY LITTLE GALLERY
-      </a>
-      <div className="w-full">
-        {isAuthenticated ? (
-          <div className="flex items-center justify-between">
-            <a className="font-bold text-neutral-900">IamArtist</a>
+        <div className="flex w-full items-center justify-between">
+          <a
+            className="text-2xl hover:cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            LLERY
+          </a>
+          <Button
+            className="lg:hidden"
+            variant={"ghost"}
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          >
+            <X />
+          </Button>
+        </div>
+        <div className="w-full">
+          {isAuthenticated ? (
+            <div className="flex items-center justify-between">
+              <a className="font-bold text-neutral-900">IamArtist</a>
+              <Button
+                variant={"ghost"}
+                className="px-1 text-red-600"
+                onClick={handleLogout}
+              >
+                로그아웃
+              </Button>
+            </div>
+          ) : (
             <Button
-              variant={"ghost"}
-              className="px-1 text-red-600"
-              onClick={handleLogout}
+              variant={"default"}
+              className="w-full rounded-full"
+              onClick={handleLoginClick}
             >
-              로그아웃
+              로그인
+            </Button>
+          )}
+        </div>
+        <div className="flex w-full flex-col">
+          <div className="flex items-center justify-between">
+            <a className="font-bold">내 전시</a>
+            <Button
+              variant={"transparent"}
+              className="p-0"
+              onClick={handleCreateClick}
+            >
+              <PlusCircle />
             </Button>
           </div>
+        </div>
+        {isAuthenticated ? (
+          <ExhibitionList items={mockExhibitionList} />
         ) : (
-          <Button
-            variant={"default"}
-            className="w-full rounded-full"
-            onClick={handleLoginClick}
-          >
-            로그인
-          </Button>
+          <div className="flex size-full items-center justify-center">
+            <a className="text-neutral-400">로그인이 필요합니다</a>
+          </div>
         )}
-      </div>
-      <div className="flex w-full flex-col">
-        <div className="flex items-center justify-between">
-          <a className="font-bold">내 전시</a>
-          <Button
-            variant={"transparent"}
-            className="p-0"
-            onClick={handleCreateClick}
-          >
-            <PlusCircle />
-          </Button>
-        </div>
-      </div>
-      {isAuthenticated ? (
-        <ExhibitionList items={mockExhibitionList} />
-      ) : (
-        <div className="flex size-full items-center justify-center">
-          <a className="text-neutral-400">로그인이 필요합니다</a>
-        </div>
-      )}
+      </aside>
     </div>
   );
 }
