26298b7a235db88be060292d673b418270220726
Gyehyun Park <gyehyunbak@gmail.com>
2024-08-17 17:13:23 +0900

    feature: 메인화면에 내 모든 전시 보기 페이지 추가, 사이드바 UI 수정

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,6 +2,7 @@ import { Button } from "./Button";
 import mainpic from "../assets/mainpic.jpg";
 import { useNavigate } from "react-router-dom";
 import { useUserStore } from "../utils/store";
+import MyExhibitions from "./MyExhibitions";
 
 export default function Landing() {
   const { isAuthenticated } = useUserStore();
@@ -19,29 +20,17 @@ export default function Landing() {
   return (
     <>
       {isAuthenticated ? (
-        <div className="flex size-full flex-col items-center justify-center">
-          <div className="flex flex-col items-center justify-center gap-5">
-            <a className="text-xl text-neutral-600">
-              새로운 전시를 만들어 보세요
-            </a>
-            <Button
-              className="w-fit rounded-full px-6"
-              onClick={handleCreateClick}
-            >
-              전시 만들기
-            </Button>
-          </div>
-        </div>
+        <MyExhibitions />
       ) : (
         <div className="flex h-full w-full flex-col justify-start gap-5 p-10">
           <div className="flex flex-col gap-0 text-[4rem] font-extrabold text-neutral-900">
-            <a>전시를 만들고</a>
-            <a>사람들에게 공유하세요</a>
+            <p>전시를 만들고</p>
+            <p>사람들에게 공유하세요</p>
           </div>
-          <a className="text-xl text-neutral-500">
+          <p className="text-xl text-neutral-500">
             그림을 업로드 하고 링크를 공유해 간단하게 전시회에 초대할 수
             있습니다
-          </a>
+          </p>
           <Button
             onClick={handleSignUpClick}
             className="w-fit rounded-full px-5 py-3"
